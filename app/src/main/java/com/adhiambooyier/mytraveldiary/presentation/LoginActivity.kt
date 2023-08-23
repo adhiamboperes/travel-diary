d19a007233128e4c2d82757a0891e3c4c9db211c
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2023-08-24 01:23:44 +0300

    Add sign in functionality
    An already registered user can login with email and password.

--- app/src/main/java/com/adhiambooyier/mytraveldiary/presentation/LoginActivity.kt
+++ app/src/main/java/com/adhiambooyier/mytraveldiary/presentation/LoginActivity.kt
@@ -2,22 +2,65 @@ package com.adhiambooyier.mytraveldiary.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.adhiambooyier.mytraveldiary.R
 import com.adhiambooyier.mytraveldiary.databinding.LoginActivityBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+private const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: LoginActivityBinding
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.login_activity)
         binding.lifecycleOwner = this
 
+        auth = Firebase.auth
+
         binding.loginButton.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            loginUser()
         }
+    }
+
+    private fun loginUser() {
+        val email = binding.loginUsernameEditText.text.toString().trim()
+        val password = binding.loginPasswordEditText.text.toString().trim()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "createUserWithEmail:success")
+                    navigateToHome()
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            navigateToHome()
+        }
+    }
+
+    private fun navigateToHome() {
+        startActivity(MainActivity.getIntent(this))
+        finish()
     }
 }
--- app/src/main/java/com/adhiambooyier/mytraveldiary/presentation/MainActivity.kt
+++ app/src/main/java/com/adhiambooyier/mytraveldiary/presentation/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.adhiambooyier.mytraveldiary.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        fun getIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+        }
+    }
 }
