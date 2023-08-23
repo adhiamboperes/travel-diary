4ba9f029d5a207c17e7d10c0033f355dc61e0150
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2023-08-23 14:30:24 +0300

    Add requisite dependencies
    Configure ksp, glide for image loading, room for offline storage, mockito and espresso for testing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    //id("com.google.devtools.ksp")
+    id("kotlin-kapt")
 }
 
 android {
@@ -13,6 +15,13 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments["room.schemaLocation"] =
+                    "$projectDir/schemas"
+            }
+        }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -40,7 +49,26 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.5.2")
+
+    implementation("androidx.room:room-runtime:2.4.3")
+    kapt("androidx.room:room-compiler:2.5.0")
+    kapt("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-rxjava2:2.4.3")
+
+    implementation("com.google.dagger:dagger:2.43.2")
+    implementation("com.google.dagger:dagger-android:2.43.2")
+    kapt("com.google.dagger:dagger-compiler:2.43.2")
+    kapt("com.google.dagger:dagger-android-processor:2.43.2")
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
+    kapt ("com.github.bumptech.glide:compiler:4.14.2")
+
     testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.room:room-testing:2.4.3")
+    testImplementation("org.mockito:mockito-inline:3.7.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
